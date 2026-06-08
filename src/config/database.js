85909d6a741a1df9d2c3e1ddd62c0adfb436976f
Eclisher <hei.harizo@gmail.com>
2026-06-08 12:49:00 +0300

    feat: enhance user registration and password reset functionality

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,11 @@ const { Pool } = pkg;
 
 export const pool = new Pool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  ssl: false,
-  
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
